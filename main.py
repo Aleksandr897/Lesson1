ba911082aae11b929e9b4e40081657593d8d0350
Aleksandr897 <markal@mail.ru>
2024-07-31 10:12:28 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+
+name = 'Это моя строка'
+print(len(name))
+print("Это моя строка"*6)
+name= 'Это моя строка'
+print(name[: : -1])
+name = 'Это моя строка'
+print(len(name))
+name="Это новая строка"
+print(name + " Это моя строка")
+
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
